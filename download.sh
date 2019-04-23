7dfd5ce1ea4ea9f26857e2ca53cbfa236489731d
Alexander Neumann <alexander@bumpern.de>
2019-04-23 13:08:11 +0200

    Update Go version

--- download.sh
+++ download.sh
@@ -4,8 +4,8 @@
 set -euo pipefail
 IFS=$'\n\t'
 
-latest="go1.11.4.linux-amd64.tar.gz"
-hash="fb26c30e6a04ad937bbc657a1b5bba92f80096af1e8ee6da6430c045a8db3a5b"
+latest="go1.12.4.linux-amd64.tar.gz"
+hash="d7d1f1f88ddfe55840712dc1747f37a790cbcaa448f6c9cf51bbe10aa65442f5"
 
 # download and install Go
 cd /usr/local
